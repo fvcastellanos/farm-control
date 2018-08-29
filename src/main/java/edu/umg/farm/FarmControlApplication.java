314f913c6ad4c473195462dc6adcf2d5fd3ba516
Francisco Castellanos <fvcastellanos@gmail.com>
2018-08-29 17:34:56 -0600

    add project template files

---
+++ src/main/java/edu/umg/farm/FarmControlApplication.java
@@ -0,0 +1,12 @@
+package edu.umg.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmControlApplication.class, args);
+	}
+}
---
+++ src/main/java/edu/umg/farm/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package edu.umg.farm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+
+        return "hello world";
+    }
+
+}
